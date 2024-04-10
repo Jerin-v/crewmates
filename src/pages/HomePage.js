f42823bd83d4a1223ee8d596bc106c9ca4ef351c
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-10 18:25:39 -0500

    add content to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React, {useState, useEffect} from "react";
 
 const HomePage = () => {
-
+    return (
+        <div>
+            <h1>Welcome to Crewmate Creator!</h1>
+            <h2>Here is where you can create your very own crewmates before sending them off to space!</h2>
+        </div>
+    )
 }
 
 export default HomePage;
