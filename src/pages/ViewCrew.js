877492b0c9952f5c5da402c6bcf8916b3ab17c89
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-10 18:39:18 -0500

    created page to view crew

---
+++ src/pages/ViewCrew.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+
+
+const ViewCrew = () => {
+    return (
+        <div>
+            <h1>View Crewmates</h1>
+            <h2>Here is where you can view all of your created crewmates!</h2>
+        </div>
+    )
+}
+
+export default ViewCrew;
