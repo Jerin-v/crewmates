dab747d78fc3e4da455f2426133c17dc76a2abba
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-10 17:48:20 -0500

    set up route to homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+
+
 
 function App() {
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<HomePage/>
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h1>Among us</h1>
+      </div>
+      {element}
     </div>
   );
 }
