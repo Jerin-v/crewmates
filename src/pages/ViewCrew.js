31a39fcb44eb0e27337abddfdf044efc487d25b8
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-11 14:54:56 -0500

    Update ViewCrew page to display crewmates dynamically

--- src/pages/ViewCrew.js
+++ src/pages/ViewCrew.js
@@ -17,6 +17,18 @@ const ViewCrew = () => {
         <div>
             <h1>View Crewmates</h1>
             <h2>Here is where you can view all of your created crewmates!</h2>
+            <div className="card">
+                {
+                    crew && crew.length > 0 ?
+                    crew.map((crewmate, index) => 
+                        <div key={index}>
+                            <h3>{crewmate.name}</h3>
+                            <p>Color: {crewmate.color}</p>
+                            <p>Speed: {crewmate.speed}</p>
+                        </div>
+                    ) : <h2>{'No Crewmates Yet 😞'}</h2>
+                }
+            </div>
         </div>
     )
 }
